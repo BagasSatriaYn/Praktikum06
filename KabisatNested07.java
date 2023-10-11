8a26bdf9822035eca36c0e3ab75a5a8152a1d5b3
BagasSatriaYn <duhjvdedhvhjufahfy@gmail.com>
2023-10-11 13:02:36 +0700

    Modifikasi percobaan 1 kabisat

--- KabisatNested07.java
+++ KabisatNested07.java
@@ -9,10 +9,14 @@ KabisatNested07{
         System.out.println("Masukkan tahun");
         tahun=input07.nextInt();
 
-        if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0)
+        if ((tahun % 400) == 0) {
             System.out.println("Tahun Kabisat");
-        } else
-            System.out.println("Bukan Tahun Kabisat");      
+
+        } else if ((tahun % 100)== 0){
+            System.out.println("Bukan Tahun Kabisat");
+        } else if ((tahun %4) ==0){
+        System.out.println("Tahun Kabisat");  
+        }else
+        System.out.println("Bukan Tahun Kabisat");      
     }
 }
